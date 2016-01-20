15b641dfa90b56628251e0403f3a3dee1dba35b6
Zim79 <simon.d.laursen.o.o@gmail.com>
2016-01-20 18:02:51 +0100

    1.0

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+	public GameObject player;
+	public GameObject exitPoint,bullet;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+
+
+
+
+
+
+
+	}
+}
