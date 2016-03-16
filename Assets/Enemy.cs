568a276509fb544b3c6198bae5aeb04d642ca787
Zim79 <simon.d.laursen.o.o@gmail.com>
2016-03-16 17:12:15 +0100

    Further level design

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,20 +6,36 @@ public class Enemy : MonoBehaviour {
 
 	public GameObject player;
 	public GameObject exitPoint,bullet;
-
+	float timer;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-	
+	void Update () {
 
+		timer += Time.deltaTime;
 
+		float distance = Vector3.Distance (player.transform.position, transform.position);
+		print (distance);
 
+		if (distance < 10) {
+			print("Bang");
 
+			if(timer > 2){
+			
+
+			GameObject temp = Instantiate(bullet) as GameObject;
+			temp.transform.position = exitPoint.transform.position;
+			
+				temp.GetComponent < Rigidbody2D > ().velocity = Vector2.left * 50;
+
+			timer = 0;
+			
+			}
+		}
 
 
 
---
+++ Assets/checkDeath.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Death") {
+			print("Death");
+			Destroy (gameObject);
+		}
+	}
+}
