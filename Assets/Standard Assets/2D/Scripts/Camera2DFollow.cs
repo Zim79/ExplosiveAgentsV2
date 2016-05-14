51b6cabca87a9b07ca9417c788867f14df259703
Zim79 <simon.d.laursen.o.o@gmail.com>
2016-05-14 19:10:14 +0200

    no message

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -11,6 +11,8 @@ namespace UnityStandardAssets._2D
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
 
+		public float YPosition = 1;
+
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
         private Vector3 m_CurrentVelocity;
@@ -45,7 +47,9 @@ namespace UnityStandardAssets._2D
             Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
-            transform.position = newPos;
+			transform.position = new Vector3 (newPos.x, YPosition, newPos.z);
+
+
 
             m_LastTargetPosition = target.position;
         }
